eab8cca818fae6fcd260ae4d1ae927e2ba15eb6a
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-18 12:34:43 +0200

    fix: rename state object

--- src/context/contacts/contactReducer.js
+++ src/context/contacts/contactReducer.js
@@ -3,13 +3,13 @@ import * as contactTypes from "../types";
 function contactReducer(state, action) {
   switch (action.type) {
     case contactTypes.ADD_CONTACT:
-      return { ...state, users: action.payload };
+      return { ...state, contacts: action.payload };
     case contactTypes.DELETE_CONTACT:
-      return { ...state, user: action.payload };
+      return { ...state, contacts: action.payload };
     case contactTypes.GET_CONTACTS:
-      return { ...state, users: action.payload };
+      return { ...state, contacts: action.payload };
     case contactTypes.UPDATE_CONTACT:
-      return { ...state, users: action.payload };
+      return { ...state, contacts: action.payload };
     default:
       return state;
   }
