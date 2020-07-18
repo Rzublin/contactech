a10dc41b4f0d0e96c941ccaeca340aa120c45ac6
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-18 12:54:37 +0200

    feat: show contact info

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,18 @@
 import React from "react";
 import StyledContact from "./StyledContact";
+import PropTypes from "prop-types";
 
-const Contact = () => {
+const Contact = ({ contact: { firstname, lastname, email } }) => {
   return (
     <div>
-      <h1>Contact</h1>
+      <p>{`Contact: ${firstname} ${lastname}`}</p>
+      <p>{`Email: ${email}`}</p>
     </div>
   );
+};
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired,
 };
 
 export default Contact;
