a11eb2f77959bbc9fb917491a7282976dabdeda7
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-19 18:20:29 +0200

    feat: update contact layout

--- src/components/Contact/StyledContact.js
+++ src/components/Contact/StyledContact.js
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const StyledContact = styled.div``;
+const StyledContact = styled.div`
+  border-bottom: 0.5px solid rgba(0, 0, 0, 0.15);
+  display: flex;
+  &:hover {
+    background-color: #eeeeee;
+  }
+  .avatar-img {
+    padding: 5px;
+    object-fit: contain;
+    //box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.5);
+  }
+  .info {
+    margin-left: 10px;
+    .fullname {
+      font-weight: bold;
+      text-transform: capitalize;
+    }
+    .email {
+    }
+  }
+  .actions {
+    .icon {
+      margin: 16px 5px;
+      transition: all 0.1s ease;
+
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.2);
+      }
+    }
+  }
+`;
+
+export default StyledContact;
