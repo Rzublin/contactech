025d45ff917a137502d9e75418b78a57a7ebf80b
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-19 18:24:18 +0200

    feat: update layout

--- src/components/Header/StyledHeader.js
+++ src/components/Header/StyledHeader.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  background-color: #006989ff;
+  color: #fff;
+  margin-bottom: 15px;
+  text-align: center;
+  padding: 2px 0px;
+  font-family: "", sans-serif;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo {
+    margin-right: 5px;
+  }
+`;
+
+export default StyledHeader;
