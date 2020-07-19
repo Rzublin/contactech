1eedf655c0be9f3d0a388bf84351a86a5da64263
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-19 18:07:53 +0200

    feat: update global style

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -1,9 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
+  $aero-blue: #c7efcfff;
+  $beige: #eef5dbff;
+  $orange-peel: #ff9d17ff;
+  $blue-sapphire: #006989ff;
+  $cg-blue: #007090ff;
+  html, body{
+    height: 100%;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+  }
+  .container{
+    max-width: 900px;
+    margin: auto;
+  }
+  .main{
+    flex: 1 0 auto;
   }
 `;
+
+export default GlobalStyle;
