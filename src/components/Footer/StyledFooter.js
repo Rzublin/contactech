c05b0552f11a8536a99a1480a6290fbd8e34adda
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-19 18:23:13 +0200

    feat: update footer style

--- src/components/Footer/StyledFooter.js
+++ src/components/Footer/StyledFooter.js
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.footer``;
+const StyledFooter = styled.footer`
+  text-align: center;
+  padding: 20px 0px;
+  border-top: 1px solid #e7e7e9;
+  margin-top: 20px;
+  color: #6e6d7a;
+  flex-shrink: 0;
+`;
+
+export default StyledFooter;
