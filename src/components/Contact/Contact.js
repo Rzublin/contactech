75cce9190979d57ef7e33b54a9927b0877a86a2b
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-19 18:20:05 +0200

    feat: add delete contact functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import StyledContact from "./StyledContact";
 import PropTypes from "prop-types";
+import ContactContext from "../../context/contacts/contactContext";
 
-const Contact = ({ contact: { firstname, lastname, email } }) => {
+const Contact = ({ contact: { firstname, lastname, email, id, avatar } }) => {
+  const { deleteContact } = useContext(ContactContext);
   return (
-    <div>
-      <p>{`Contact: ${firstname} ${lastname}`}</p>
-      <p>{`Email: ${email}`}</p>
-    </div>
+    <StyledContact>
+      <img
+        className="avatar-img"
+        src={avatar}
+        alt={`Avatar from ${firstname} ${lastname}`}
+      />
+      <div className="info">
+        <p className="fullname">{`${firstname} ${lastname}`}</p>
+        <p className="email">{`${email}`}</p>
+      </div>
+      <div className="actions">
+        <img className="icon" src="/edit-icon.svg" alt="Edit Icon" />
+        <img
+          className="icon"
+          src="/delete-icon.svg"
+          alt="Delete Icon"
+          onClick={() => deleteContact(id)}
+        />
+      </div>
+    </StyledContact>
   );
 };
 
