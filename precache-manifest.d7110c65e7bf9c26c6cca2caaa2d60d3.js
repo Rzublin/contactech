c8cae5be6ce7544a7fafd45a2a17219b962ca92c
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-20 07:05:00 +0200

    Updates

---
+++ precache-manifest.d7110c65e7bf9c26c6cca2caaa2d60d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc6e8f56bd7a5c3bf0ab7a44b4583ed",
+    "url": "/contactech/index.html"
+  },
+  {
+    "revision": "c0039b6b1acd4ea5c1e9",
+    "url": "/contactech/static/js/2.aa988a5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactech/static/js/2.aa988a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b0c6b9a5ab64bcdbda",
+    "url": "/contactech/static/js/main.4cb70363.chunk.js"
+  },
+  {
+    "revision": "d2cade1ce097308bae32",
+    "url": "/contactech/static/js/runtime-main.962ae39a.js"
+  }
+]);
