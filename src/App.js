b49bd466d53c87909b8f3009df55b3504caa5cb2
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-20 06:54:05 +0200

    feat: add edit modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import ContactList from "./components/ContactList/ContactList";
 import Footer from "./components/Footer/Footer";
 import ContactState from "./context/contacts/ContactState";
-import ContactAdd from "./components/ContactAdd/ContactAdd";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [isUpdateMode, setIsUpdateMode] = useState(false);
+  const [contact, setContact] = useState("");
+
+  const toggleModal = () => {
+    setIsUpdateMode(!isUpdateMode);
+  };
+  const editContact = (firstname, lastname, email, id, avatar) => {
+    setIsUpdateMode(!isUpdateMode);
+    setContact({ firstname, lastname, email, id, avatar });
+  };
   return (
     <ContactState>
       <Header />
+      {isUpdateMode && (
+        <Modal toggleModal={toggleModal}>
+          <ContactForm
+            actionType="UPDATE"
+            contact={contact}
+            toggleModal={toggleModal}
+          />
+        </Modal>
+      )}
       <div className="main container">
-        <ContactList />
-        <ContactAdd />
+        <ContactList editContact={editContact} />
+        <ContactForm actionType="ADD" />
       </div>
       <Footer />
     </ContactState>
