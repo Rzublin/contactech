0be9ebb0bdb26877b617e2a566fffd6af77a1ff2
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-20 06:51:56 +0200

    feat: add proptype check

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,15 +2,22 @@ import React, { useContext } from "react";
 import StyledContactList from "./StyledContactList";
 import Contact from "../Contact/Contact";
 import ContactContext from "../../context/contacts/contactContext";
+import PropTypes from "prop-types";
 
-const ContactList = () => {
+const ContactList = ({ editContact }) => {
   const { state } = useContext(ContactContext);
   return (
     <div>
       {state.length > 0 &&
-        state.map((el) => <Contact key={el.id} contact={el} />)}
+        state.map((el) => (
+          <Contact key={el.id} contact={el} editContact={editContact} />
+        ))}
     </div>
   );
+};
+
+ContactList.propTypes = {
+  editContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
